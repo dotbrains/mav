d4a9498e86aa99bcb43f53ecb8c63b0a879550e7
Nicholas Adamou <10106289+nicholasadamou@users.noreply.github.com>
2026-07-21 03:40:15 -0400

    refactor: extract settings keymap page

--- crates/settings_ui/src/page_data.rs
+++ crates/settings_ui/src/page_data.rs
@@ -67,10 +67,12 @@ mod appearance_editor;
 mod appearance_fonts;
 mod appearance_theme;
 mod general;
+mod keymap;
 mod version_control;
 
 use appearance::appearance_page;
 use general::{developer_page, general_page};
+use keymap::keymap_page;
 use version_control::version_control_page;
 
 pub(crate) fn settings_data(cx: &App) -> Vec<SettingsPage> {
@@ -91,97 +93,6 @@ pub(crate) fn settings_data(cx: &App) -> Vec<SettingsPage> {
         network_page(),
         developer_page(cx),
     ]
-}
-
-fn keymap_page() -> SettingsPage {
-    fn keybindings_section() -> [SettingsPageItem; 2] {
-        [
-            SettingsPageItem::SectionHeader("Keybindings"),
-            SettingsPageItem::ActionLink(ActionLink {
-                title: "Edit Keybindings".into(),
-                description: Some("Customize keybindings in the keymap editor.".into()),
-                button_text: "Open Keymap".into(),
-                on_click: Arc::new(|settings_window, window, cx| {
-                    let Some(original_window) = settings_window.original_window else {
-                        return;
-                    };
-                    original_window
-                        .update(cx, |_workspace, original_window, cx| {
-                            original_window
-                                .dispatch_action(mav_actions::OpenKeymap.boxed_clone(), cx);
-                            original_window.activate_window();
-                        })
-                        .ok();
-                    window.remove_window();
-                }),
-                files: USER,
-            }),
-        ]
-    }
-
-    fn base_keymap_section() -> [SettingsPageItem; 2] {
-        [
-            SettingsPageItem::SectionHeader("Base Keymap"),
-            SettingsPageItem::SettingItem(SettingItem {
-                title: "Base Keymap",
-                description: "The name of a base set of key bindings to use.",
-                field: Box::new(SettingField {
-                    organization_override: None,
-                    json_path: Some("base_keymap"),
-                    pick: |settings_content| settings_content.base_keymap.as_ref(),
-                    write: |settings_content, value, _| {
-                        settings_content.base_keymap = value;
-                    },
-                }),
-                metadata: Some(Box::new(SettingsFieldMetadata {
-                    should_do_titlecase: Some(false),
-                    ..Default::default()
-                })),
-                files: USER,
-            }),
-        ]
-    }
-
-    fn modal_editing_section() -> [SettingsPageItem; 3] {
-        [
-            SettingsPageItem::SectionHeader("Modal Editing"),
-            SettingsPageItem::SettingItem(SettingItem {
-                title: "Vim Mode",
-                description: "Enable Vim mode and key bindings.",
-                field: Box::new(SettingField {
-                    organization_override: None,
-                    json_path: Some("vim_mode"),
-                    pick: |settings_content| settings_content.vim_mode.as_ref(),
-                    write: write_vim_mode,
-                }),
-                metadata: None,
-                files: USER,
-            }),
-            SettingsPageItem::SettingItem(SettingItem {
-                title: "Helix Mode",
-                description: "Enable Helix mode and key bindings.",
-                field: Box::new(SettingField {
-                    organization_override: None,
-                    json_path: Some("helix_mode"),
-                    pick: |settings_content| settings_content.helix_mode.as_ref(),
-                    write: write_helix_mode,
-                }),
-                metadata: None,
-                files: USER,
-            }),
-        ]
-    }
-
-    let items: Box<[SettingsPageItem]> = concat_sections!(
-        keybindings_section(),
-        base_keymap_section(),
-        modal_editing_section(),
-    );
-
-    SettingsPage {
-        title: "Keymap",
-        items,
-    }
 }
 
 fn editor_page() -> SettingsPage {
