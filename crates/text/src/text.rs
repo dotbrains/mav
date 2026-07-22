fbb14c2581f29f444b139c76e48ba1f52c91b112
Nicholas Adamou <10106289+nicholasadamou@users.noreply.github.com>
2026-07-22 03:26:36 -0400

    refactor: split text operation types

--- crates/text/src/text.rs
+++ crates/text/src/text.rs
@@ -11,10 +11,11 @@ mod selection;
 pub mod subscription;
 #[cfg(test)]
 mod tests;
+mod types;
 mod undo_map;
 
 pub use anchor::*;
-use anyhow::{Context as _, Result};
+use anyhow::Result;
 use clock::Lamport;
 pub use clock::ReplicaId;
 use collections::{HashMap, HashSet};
@@ -31,11 +32,9 @@ pub use selection::*;
 use smallvec::SmallVec;
 use std::{
     cmp::{self, Ordering, Reverse},
-    fmt::Display,
     future::Future,
     iter::Iterator,
-    num::NonZeroU64,
-    ops::{self, Deref, Range, Sub},
+    ops::{self, Deref, Range},
     str,
     sync::Arc,
     time::{Duration, Instant},
@@ -43,6 +42,7 @@ use std::{
 pub use subscription::*;
 pub use sum_tree::Bias;
 use sum_tree::{Dimensions, FilterCursor, SumTree, Summary, TreeMap, TreeSet};
+pub use types::*;
 use undo_map::UndoMap;
 use util::debug_panic;
 
@@ -66,48 +66,6 @@ pub struct Buffer {
     subscriptions: Topic<usize>,
     edit_id_resolvers: HashMap<clock::Lamport, Vec<oneshot::Sender<()>>>,
     wait_for_version_txs: Vec<(clock::Global, oneshot::Sender<()>)>,
-}
-
-#[repr(transparent)]
-#[derive(Clone, Copy, Debug, Hash, PartialEq, PartialOrd, Ord, Eq)]
-pub struct BufferId(NonZeroU64);
-
-impl Display for BufferId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-impl From<NonZeroU64> for BufferId {
-    fn from(id: NonZeroU64) -> Self {
-        BufferId(id)
-    }
-}
-
-impl BufferId {
-    /// Returns Err if `id` is outside of BufferId domain.
-    pub fn new(id: u64) -> anyhow::Result<Self> {
-        let id = NonZeroU64::new(id).context("Buffer id cannot be 0.")?;
-        Ok(Self(id))
-    }
-
-    /// Increments this buffer id, returning the old value.
-    /// So that's a post-increment operator in disguise.
-    pub fn next(&mut self) -> Self {
-        let old = *self;
-        self.0 = self.0.saturating_add(1);
-        old
-    }
-
-    pub fn to_proto(self) -> u64 {
-        self.into()
-    }
-}
-
-impl From<BufferId> for u64 {
-    fn from(id: BufferId) -> Self {
-        id.0.get()
-    }
 }
 
 #[derive(Clone)]
@@ -191,48 +149,6 @@ struct Edits<'a, D: TextDimension, F: FnMut(&FragmentSummary) -> bool> {
     buffer_id: BufferId,
 }
 
-#[derive(Clone, Debug, Default, Eq, PartialEq)]
-pub struct Edit<D> {
-    pub old: Range<D>,
-    pub new: Range<D>,
-}
-impl<D> Edit<D>
-where
-    D: PartialEq,
-{
-    pub fn is_empty(&self) -> bool {
-        self.old.start == self.old.end && self.new.start == self.new.end
-    }
-}
-
-impl<D, DDelta> Edit<D>
-where
-    D: Sub<D, Output = DDelta> + Copy,
-{
-    pub fn old_len(&self) -> DDelta {
-        self.old.end - self.old.start
-    }
-
-    pub fn new_len(&self) -> DDelta {
-        self.new.end - self.new.start
-    }
-}
-
-impl<D1, D2> Edit<(D1, D2)> {
-    pub fn flatten(self) -> (Edit<D1>, Edit<D2>) {
-        (
-            Edit {
-                old: self.old.start.0..self.old.end.0,
-                new: self.new.start.0..self.new.end.0,
-            },
-            Edit {
-                old: self.old.start.1..self.old.end.1,
-                new: self.new.start.1..self.new.end.1,
-            },
-        )
-    }
-}
-
 #[derive(Eq, PartialEq, Clone, Debug)]
 struct Fragment {
     id: Locator,
@@ -281,27 +197,6 @@ struct InsertionFragment {
 struct InsertionFragmentKey {
     timestamp: clock::Lamport,
     split_offset: u32,
-}
-
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub enum Operation {
-    Edit(EditOperation),
-    Undo(UndoOperation),
-}
-
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct EditOperation {
-    pub timestamp: clock::Lamport,
-    pub version: clock::Global,
-    pub ranges: Vec<Range<FullOffset>>,
-    pub new_text: Vec<Arc<str>>,
-}
-
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct UndoOperation {
-    pub timestamp: clock::Lamport,
-    pub version: clock::Global,
-    pub counts: HashMap<clock::Lamport, u32>,
 }
 
 impl Buffer {
